7d3d6f66025462158671a7c78377de3a39e022e4
Johan Lindskogen <johan.lindskogen@gmail.com>
2024-12-04 20:16:21 +0100

    Add FixedLineLengthBuffer abstraction to eliminate allocations

--- day04/solution.zig
+++ day04/solution.zig
@@ -3,30 +3,24 @@ const Allocator = std.mem.Allocator;
 
 const Pair = std.meta.Tuple(&.{ isize, isize });
 
-const dirs1: [8]Pair = [_]Pair{ .{ -1, 0 }, .{ -1, -1 }, .{ -1, 1 }, .{ 1, 0 }, .{ 1, -1 }, .{ 1, 1 }, .{ 0, -1 }, .{ 0, 1 } };
+const dirs1 = [_]Pair{ .{ -1, 0 }, .{ -1, -1 }, .{ -1, 1 }, .{ 1, 0 }, .{ 1, -1 }, .{ 1, 1 }, .{ 0, -1 }, .{ 0, 1 } };
 
 const WORD = "XMAS";
 
-fn solve1(input: []const u8, alloc: Allocator) !usize {
-    var rowsIter = std.mem.tokenizeScalar(u8, input, '\n');
-    var rows = std.ArrayList([]const u8).init(alloc);
-    defer rows.deinit();
-
-    while (rowsIter.next()) |row| {
-        try rows.append(row);
-    }
+fn solve1(input: []const u8, _: Allocator) !usize {
+    const rows = try toFixedLineLengthBuffer(input);
 
     var count: usize = 0;
 
-    for (rows.items, 0..) |r, y| {
-        for (r, 0..) |_, x| {
+    for (0..rows.len()) |y| {
+        for (0..rows.line_len()) |x| {
             dir: for (dirs1) |p| {
                 inline for (0..4) |d| {
                     const yd: isize = @as(isize, @intCast(y)) + p[0] * d;
                     const xd: isize = @as(isize, @intCast(x)) + p[1] * d;
 
-                    if (yd >= 0 and yd < rows.items.len and xd >= 0 and xd < rows.items[y].len) {
-                        if (rows.items[@intCast(yd)][@intCast(xd)] != WORD[d]) {
+                    if (rows.get_signed(yd, xd)) |c| {
+                        if (c != WORD[d]) {
                             continue :dir;
                         }
                     } else {
@@ -41,29 +35,23 @@ fn solve1(input: []const u8, alloc: Allocator) !usize {
     return count;
 }
 
-const dirs2: [4][2]Pair = [4][2]Pair{
-    [2]Pair{ .{ -1, -1 }, .{ 1, 1 } },
-    [2]Pair{ .{ -1, 1 }, .{ 1, -1 } },
-    [2]Pair{ .{ 1, -1 }, .{ -1, 1 } },
-    [2]Pair{ .{ 1, 1 }, .{ -1, -1 } },
+const dirs2 = [_][2]Pair{
+    [_]Pair{ .{ -1, -1 }, .{ 1, 1 } },
+    [_]Pair{ .{ -1, 1 }, .{ 1, -1 } },
+    [_]Pair{ .{ 1, -1 }, .{ -1, 1 } },
+    [_]Pair{ .{ 1, 1 }, .{ -1, -1 } },
 };
 
 const WORD2 = "MS";
 
-fn solve2(input: []const u8, alloc: Allocator) !usize {
-    var rowsIter = std.mem.tokenizeScalar(u8, input, '\n');
-    var rows = std.ArrayList([]const u8).init(alloc);
-    defer rows.deinit();
-
-    while (rowsIter.next()) |row| {
-        try rows.append(row);
-    }
+fn solve2(input: []const u8, _: Allocator) !usize {
+    const rows = try toFixedLineLengthBuffer(input);
 
     var count: usize = 0;
 
-    for (rows.items, 0..) |r, y| {
-        for (r, 0..) |_, x| {
-            if (rows.items[y][x] != 'A') {
+    for (0..rows.len()) |y| {
+        for (0..rows.line_len()) |x| {
+            if (rows.get(y, x) != 'A') {
                 continue;
             }
 
@@ -73,8 +61,8 @@ fn solve2(input: []const u8, alloc: Allocator) !usize {
                     const yd: isize = @as(isize, @intCast(y)) + p[0];
                     const xd: isize = @as(isize, @intCast(x)) + p[1];
 
-                    if (yd >= 0 and yd < rows.items.len and xd >= 0 and xd < rows.items[y].len) {
-                        if (rows.items[@intCast(yd)][@intCast(xd)] != WORD2[idx]) {
+                    if (rows.get_signed(yd, xd)) |c| {
+                        if (c != WORD2[idx]) {
                             continue :dir;
                         }
                     } else {
@@ -111,6 +99,96 @@ pub fn main() !void {
     const res2 = try solve2(buffer, gpa);
 
     std.debug.print("Part 2: {d}\n", .{res2});
+}
+
+
+const FixedLineLengthBuffer = struct {
+  line_length: usize,
+  total_lines: usize,
+  text: []const u8,
+
+  pub fn get_signed(self: @This(), row: isize, col: isize) ?u8 {
+    if (row < 0 or col < 0) {
+      return null;
+    } else {
+      return self.get(@intCast(row), @intCast(col));
+    }
+  }
+
+  pub fn get(self: @This(), row: usize, col: usize) ?u8 {
+    if (row < self.total_lines and col < self.line_length) {
+      // Account for '\n' at end of line
+      const index = row * (self.line_length + 1) + col;
+      return self.text[index];
+    }
+
+    return null;
+  }
+
+  pub fn len(self: @This()) usize {
+    return self.total_lines;
+  }
+
+  pub fn line_len(self: @This()) usize {
+    return self.line_length;
+  }
+
+  pub fn lines_iter(buf: @This()) LinesIter {
+
+    return .{
+      .line_length = buf.line_length,
+      .total_lines = buf.total_lines,
+      .text = buf.text
+    };
+  }
+};
+
+
+const LinesIter = struct {
+  index: usize = 0,
+  line_length: usize,
+  total_lines: usize,
+  text: []const u8,
+  pub fn next(self: *@This()) ?[]const u8 {
+    const start = self.index * (self.line_length + 1);
+    const end = start + self.line_length;
+    if (end > self.text.len) {
+      return null;
+    }
+    self.index += 1;
+    return self.text[start..end];
+  }
+};
+
+pub fn toFixedLineLengthBuffer(text: []const u8) !FixedLineLengthBuffer {
+  var count = std.mem.count(u8, text, "\n");
+  if (text[text.len - 1] != '\n') {
+    count += 1;
+  }
+  const line_length = std.mem.indexOfScalar(u8, text, '\n') orelse return error.NoNewlineFound;
+
+  return .{ .line_length = line_length, .total_lines = count, .text = text };
+}
+
+test "fixedLineLengthBuffer" {
+  const input =
+    \\MMMSXXMASM
+    \\MSAMXMSMSA
+    \\AMXSXMAAMM
+    \\MSAMASMSMX
+    \\XMASAMXAMM
+    \\XXAMMXXAMA
+    \\SMSMSASXSS
+    \\SAXAMASAAA
+    \\MAMMMXMMMM
+    \\MXMXAXMASX
+    \\MXMXAXMASX
+    ;
+
+  const fixedBuffer = try toFixedLineLengthBuffer(input);
+
+  try std.testing.expectEqual(10, fixedBuffer.line_len());
+  try std.testing.expectEqual(11, fixedBuffer.len());
 }
 
 test "part 1 - simple" {
